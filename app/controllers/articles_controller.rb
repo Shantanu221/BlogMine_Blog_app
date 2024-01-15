3e62bd30ee9c0134dd267d716ef73a93c79050e0
Shantanu <shantanu.ingale22@gmail.com>
2024-01-15 20:17:44 +0530

    user controller and model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -17,7 +17,7 @@ class ArticlesController < ApplicationController
 
     def create
       @article = Article.new(article_params)
-      @article.user = User.first
+      # @article.user = User.first
       if @article.save
       # redirect_to article_path(@article)
         redirect_to @article
