c03854d4516dc923f3f2dc413971b0960e399f6e
Shantanu <shantanu.ingale22@gmail.com>
2024-01-16 18:34:00 +0530

    adding admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_15_150554) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_16_130258) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "description"
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_15_150554) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
 end
